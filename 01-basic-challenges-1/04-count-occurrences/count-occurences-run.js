da9abf9775ad627e6cbbe4cd9e2a949117de3552
Victoria Munteanu <munteanuvictoria1@gmail.com>
2024-07-10 16:24:42 +0200

    update

--- 01-basic-challenges-1/04-count-occurrences/count-occurences-run.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurences-run.js
@@ -1,5 +1,5 @@
-const countOccurrences = require('./count-occurrences');
+const countOccurrences = require("./count-occurrences");
 
-const result = countOccurrences('hellLo world', 'l');
+const result = countOccurrences("hellLo world", "l");
 
 console.log(result);
